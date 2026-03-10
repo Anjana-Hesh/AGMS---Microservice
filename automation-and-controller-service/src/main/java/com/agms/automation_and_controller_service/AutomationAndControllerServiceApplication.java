898c50cdddda7439bcc7e0f138b361ce25432f74
LENOVO <anjanaheshan676@gmail.com>
2026-03-10 13:07:34 +0530

    Automation and controller service initialized

---
+++ automation-and-controller-service/src/main/java/com/agms/automation_and_controller_service/AutomationAndControllerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agms.automation_and_controller_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationAndControllerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationAndControllerServiceApplication.class, args);
+	}
+
+}
